1a8986888be0067c516c7a2203420be5fafab6e6
GeorgeJipa <GeorgeJipa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-13 08:19:02 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tweet-this-button/trunk@275146 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Tweet This! Button ===
+Contributors: georgejipa
+Donate link: http://www.georgejipa.com
+Tags: twitter, tweet this, twitter button
+Requires at least: 2.5
+Tested up to: 3.0
+Stable tag: 1.0
+Version: 1.0
+
+== Description ==
+
+By using this plugin you can display on your blog, "Tweet this! Button", the official button from Twitter
+
+== Installation ==
+
+1. Upload the plugin into "/wp-content/plugins/" directory
+2. Active it from "Plugins" dashboard
+3. Insert the following function into your blog template (you should change georgejipa with your own twitter account)
+
+if(function_exists("tweet_button")) { echo tweet_button('georgejipa'); }
+
+== Frequently Asked Questions ==
+
+Q: http://georgejipa.com/plugins/tweet-this-button/
+
+== Upgrade Notice ==
+
+No updates!
+
+== Changelog ==
+
+No updates!
+
---
+++ tweet_button.php
@@ -0,0 +1,23 @@
+<?php
+  /**
+   *  Plugin Name: Tweet This! Button
+   *  Version: 1.0
+   *  Plugin URI: http://georgejipa.com/plugins/tweet-this-button/
+   *  Description: By using this plugin you can display on your blog, "Tweet this! Button", the official button from Twitter
+   *  Author: George Jipa
+   *  Author URI: http://georgejipa.com/        
+   **/
+   
+  function tweet_button_js(){
+	 echo '<script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>';
+  }
+
+  function tweet_button($account, $data_count = 'horizontal'){
+	  $button  = '<div>';
+    $button .= '<a href="http://twitter.com/share" class="twitter-share-button" data-url="'.get_permalink().'" data-via="'.$account.'" data-text="'.get_the_title().'" data-count="'.$data_count.'">Tweet</a>';
+    $button .= '</div>';
+	  return $button;
+  }
+
+  add_action('wp_head', 'tweet_button_js');
+?>
